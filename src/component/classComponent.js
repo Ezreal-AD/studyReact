ce5ed6a6e04f4469a02f5133ad61701ed1156ff5
Wbx <wbx2023@gmail.com>
2023-04-01 01:22:09 +0800

    react事件绑定

--- src/component/classComponent.js
+++ src/component/classComponent.js
@@ -4,12 +4,23 @@ import '../css/index.css'
 class Top extends Component {
     render() {
         return (
-            <div>top</div>
+            <div><button onClick={() => this.clickButton('111')}>点击按钮</button>
+            <button onClick={this.showEvent}>点击按钮</button>
+            </div>
         )
+    }
+    clickButton(value) {
+        console.log(value)
+    }
+    showEvent(event){
+        console.log(event)  //事件代理 接收到事件
     }
 }
 
-const Content = () =>  <div className='bgContent'>{12+20}</div>
+const Content = () => <div className='bgContent' onClick={() => {
+    console.log('gett?')
+}}>{12 + 20}
+</div>
 
 function Bottom() {
     return (
