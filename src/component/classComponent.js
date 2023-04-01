bf73c2356eb99a1255bcec7c5f45859e9ed21a60
Wbx <wbx2023@gmail.com>
2023-04-02 02:10:37 +0800

    今日学习：ref绑定和获取，state状态 以及setState修改状态

--- src/component/classComponent.js
+++ src/component/classComponent.js
@@ -2,21 +2,41 @@ import React, { Component } from 'react'
 import '../css/index.css'
 
 class Top extends Component {
+    refValue = React.createRef()
     render() {
         return (
-            <div><button onClick={() => this.clickButton('111')}>点击按钮</button>
-            <button onClick={this.showEvent}>点击按钮</button>
+            <div className='topBox'><button onClick={() => this.clickButton('111')}>点击按钮</button>
+                <button onClick={this.showEvent}>点击按钮</button>
+                <button ref={this.refValue} onClick={() => this.showRef()}>展示ref</button>
+                <TopTitle></TopTitle>
             </div>
         )
     }
     clickButton(value) {
         console.log(value)
     }
-    showEvent(event){
+    showEvent(event) {
         console.log(event)  //事件代理 接收到事件
     }
+    showRef() {
+        console.log(this.refValue.current, 'refValue')
+    }
 }
-
+class TopTitle extends Component {
+    state = {
+        isShow: true,
+        spanValue: '是的'
+    }
+    render() {
+        return (<div>
+            <span>{this.state.isShow ? '214' : 'xxx'}</span>
+            <div onClick={() => this.changeSpan()}>切换</div>
+        </div>)
+    }
+    changeSpan() {
+        this.setState({ isShow: !this.state.isShow })
+    }
+}
 const Content = () => <div className='bgContent' onClick={() => {
     console.log('gett?')
 }}>{12 + 20}
